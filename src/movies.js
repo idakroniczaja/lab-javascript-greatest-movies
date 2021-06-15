2f14e8497bd85f81e0847e47648275832e4c9cdd
Ida <so.kida@hotmail.com>
2021-06-14 20:34:05 -0400

    iteration 7

--- src/movies.js
+++ src/movies.js
@@ -1,49 +1,98 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 function getAllDirectors (arr){
-const allDirectors = arr.map(elem=>elem.director)
-const filteredDirectors = allDirectors.filter((elem, i)=>allDirectors.indexOf(elem)===i)
-return filteredDirectors
+  let allDirectors = arr.map(movie=>movie.director);
+  let filteredDirectors = allDirectors.filter((each, i)=> allDirectors.indexOf(each)===i);
+  return filteredDirectors
 }
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(arr){
-const moviesSpielberg = arr.filter(elem=>elem.director === "Steven Spielberg" && elem.genre.includes("Drama"))
-return moviesSpielberg.length
+  let moviesSpielberg = arr.filter(movie=> movie.director==="Steven Spielberg")
+  let dramaMovies = moviesSpielberg.filter(movie=>movie.genre.includes('Drama'))
+  return dramaMovies.length
 }
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 function ratesAverage(arr){
+  let movieRates = arr.map(movie=>{
+   if(!movie.rate){
+     return movie.rate =0
+   }else {
+     return movie.rate
+   }
+   
+  })
 
-const avgRates = arr.reduce((accum, currVal)=> {return accum + currVal.rate;}, 0)/arr.length;
-if (!arr.length) return 0;
-else if (arr.rate===0) return parseFloat(avgRates.toFixed(2));
-else return parseFloat(avgRates.toFixed(2))
+  let averageScore = Number((movieRates.reduce((acc,vall)=>acc+vall,0)/movieRates.length).toFixed(2))
+if(!arr.length){
+  return 0
+}else {
+  return averageScore
+}
 
   }
+
+
+
+
+  
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(arr){
-  const dramaMovies = arr.filter((elem=> elem.genre.includes("Drama")));
-return ratesAverage(dramaMovies)
+  let dramaMovies = arr.filter(movie => movie.genre.includes("Drama") );
+  let dramaRates = dramaMovies.reduce((sum, movie)=>{
+    return sum + movie.rate
+  },0)/dramaMovies.length
+
+  if(dramaMovies.length===0){
+    return 0
+  }else return Number(dramaRates.toFixed(2))
 }
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(arr){
-const releasYear =[...arr].sort((a,b)=>a.year-b.year);
-let sameYear = releasYear.filter((elem, i)=>releasYear.indexOf(elem) === i).sort
-const sortYear = [...sameYears].sort ((a,b)=>a.title.localeCompare(b.title))
-return sameYears
+  let orderYear =  [...arr].sort ((a,b)=>a.title?.localeCompare(b.title)).sort((a,b)=>a.year-b.year)
+  return orderYear
+
 }
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr){
-const sortByName = [...arr].sort((a,b)=>a.title.localeCompare(b.title));
-if (sortByName.length>20)
-return (sortByName.slice(0, 3));
-else return sortByName
+  return [...arr].sort((a,b)=>a.title.localeCompare(b.title)).map(movie=>movie.title).filter((movie, i)=>i<20)
 }
+
+
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes(arr){
-let splitArr = arr.map(elem=>elem.duration.split(' '))
-let replaceAndJoinArr = splitArr.map(elem=>elem[0].replace('h',' ')+elem[1].replace('min', ''));
-let sumNumArr = replaceAndJoinArr.map(elem=>elem.split(' ')).map(elem=>Number(elem[0])*60+Number(elem[1]));
-let finalArr = arr.map((elem,i)=>elem.duration=sumNumArr[i])
-return arr
-}
+function turnHoursToMinutes(array){
+
+
+  const caluclateMinutes = (movieDuration) => {
+    if (String(movieDuration).includes('h')){
+      return String(movieDuration).split(' ').map((elem,index)=>{
+        if(index===0){
+          return parseInt(elem)*60
+        }else {
+            return parseInt(elem)
+        }
+        }).reduce((acc,vall)=>acc+vall,0)
+  
+    } else{
+      if(parseInt(movieDuration)>60){
+        return parseInt(movieDuration)/60
+      }else {
+        return parseInt(movieDuration)
+      }
+    }
+  
+    }
+ 
+  
+  let allMovieDurations = [...array].map(movie=>{
+     movie.duration = caluclateMinutes(movie.duration)
+    return movie
+  })
+  
+  return allMovieDurations
+  }
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(){
+
+}
